cad22a7d97171f2497757fadf85f6a0b9032fb95
JesusL1 <JesusL1@users.noreply.github.com>
2020-10-19 00:25:03 -0700

    incorporated localStorage so a user can save a product

--- popup/pricewatcher.js
+++ popup/pricewatcher.js
@@ -1,14 +1,19 @@
-var form = document.getElementById("form")
-form.onsubmit = function() {
-    alert("The form was submitted")
-}
-
-browser.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    
-    browser.tabs.sendMessage(tabs[0].id, {greeting: "hello i'm from pw.js"}, function(response) {
-        document.getElementById("productName").value = response.productInfo["name"]
-        document.getElementById('productPrice').value = response.productInfo["price"]
+browser.tabs.query({active: true, currentWindow: true}, function(tabs) { 
+    browser.tabs.sendMessage(tabs[0].id, {greeting: "i'm a message from page-script"}, function(response) {
+        document.getElementById("productName").value  = response.productInfo["name"]
+        document.getElementById("productPrice").value = response.productInfo["price"]
         document.getElementById("productURL").value = response.productInfo["url"]
     })
+});
 
-  });
+
+var form = document.getElementById("form")
+form.onsubmit = function() {
+    var productName = document.getElementById("productName").value 
+    var productPrice = document.getElementById("productPrice").value
+    var productURL = document.getElementById("productURL").value
+    localStorage.setItem(productURL, JSON.stringify([productName, productPrice]))
+    var arr = JSON.parse(localStorage.getItem(productURL))
+    console.log(arr)
+    alert("The form was submitted: " + productName)
+}
