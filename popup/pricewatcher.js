f87bd4bc257664a61a797dc0e7043f49abdad93f
JesusL1 <JesusL1@users.noreply.github.com>
2020-10-19 21:59:11 -0700

    added watchlist page

--- popup/pricewatcher.js
+++ popup/pricewatcher.js
@@ -8,16 +8,13 @@ browser.tabs.query({active: true, currentWindow: true}, function(tabs) {
 
 
 var form = document.getElementById("form")
-form.onsubmit = function(event) {
-    event.preventDefault()
+form.onsubmit = function() {
     var productName = document.getElementById("productName").value 
     var productPrice = document.getElementById("productPrice").value
     var productURL = document.getElementById("productURL").value
     localStorage.setItem(productURL, JSON.stringify([productName, productPrice]))
     var arr = JSON.parse(localStorage.getItem(productURL))
     console.log(arr)
-    location.replace("/popup/watchlist.html")
-   
     //alert("The form was submitted: " + productName)
     
 }
