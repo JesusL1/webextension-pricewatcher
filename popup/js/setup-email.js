6175b8d03577c5d4900b3efa78b83798bb3c5b90
JesusL1 <JesusL1@users.noreply.github.com>
2020-11-04 22:55:11 -0800

    Update "Update-Email" UI

--- popup/js/setup-email.js
+++ popup/js/setup-email.js
@@ -5,7 +5,7 @@ if (userEmail !== null)
     document.getElementById("current_email").innerText = userEmail
 }
 
-var form = document.getElementById("form")
+var form = document.getElementById("emailForm")
 form.onsubmit = function() {
     var email = document.getElementById("Email").value 
     localStorage.setItem('userEmail', email)   
